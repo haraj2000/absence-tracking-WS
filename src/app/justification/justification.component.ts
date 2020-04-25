fc3ae06e38ed178117c47c4bcb0b638e71685221
haraj2000 <haja.senabo@mail.com>
2020-04-25 13:32:19 +0000

    add service and model

---
+++ src/app/justification/justification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-justification',
+  templateUrl: './justification.component.html',
+  styleUrls: ['./justification.component.css']
+})
+export class JustificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
