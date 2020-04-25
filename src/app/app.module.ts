3733aef2d98752a2708080717356e1be447a8656
haraj2000 <haja.senabo@mail.com>
2020-04-25 13:23:35 +0000

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfilComponent } from './profil/profil.component';
+import { SemestreComponent } from './semestre/semestre.component';
+import { ModuleComponent } from './module/module.component';
+import { GroupeComponent } from './groupe/groupe.component';
+import { AbsenceComponent } from './absence/absence.component';
+import { JustificationComponent } from './justification/justification.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { EnseignantComponent } from './enseignant/enseignant.component';
+import { FiliereComponent } from './filiere/filiere.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfilComponent,
+    SemestreComponent,
+    ModuleComponent,
+    GroupeComponent,
+    AbsenceComponent,
+    JustificationComponent,
+    EtudiantComponent,
+    EnseignantComponent,
+    FiliereComponent
   ],
   imports: [
     BrowserModule
